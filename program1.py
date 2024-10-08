258ccf2febc489e5a28557b2b46005ddf5ccb8bb
umesh yadav <yadavumeshkumar815@gmail.com>
2024-10-08 15:34:59 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -8,14 +8,10 @@ class Solution(object):
         bracket_map = {')': '(', '}': '{', ']': '['}
         for char in s:
             if char in bracket_map:
-                # Pop the top element from the stack if it's not empty, else assign a dummy value
                 top_element = stack.pop() if stack else '#'
-                
-                # Check if the popped element is the matching opening bracket
                 if bracket_map[char] != top_element:
                     return False
             else:
-                # If it's an opening bracket, push it onto the stack
                 stack.append(char)
         
         # If the stack is empty, all opening brackets were properly closed
