27e1e49506417ad7fa147482a84685ce93f1b71c
umesh yadav <yadavumeshkumar815@gmail.com>
2024-10-08 15:17:35 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,6 +4,7 @@ class Solution(object):
         bracket_map = {')': '(', '}': '{', ']': '['}
        for char in s:
          if char in bracket_map:
+             top_element = stack.pop() if stack else '#'
         pass
 
 
